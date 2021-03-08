5b11f1ba88d9c3929c799dc3b8d0c20e9aaf1cc2
Gustavo Ridgue <guferrari007@gmail.com>
2021-03-08 12:02:31 -0300

    Implement pagination

--- src/main/java/br/com/devplus/forum/controller/TopicosController.java
+++ src/main/java/br/com/devplus/forum/controller/TopicosController.java
@@ -8,8 +8,10 @@ import br.com.devplus.forum.modelo.Curso;
 import br.com.devplus.forum.modelo.Topico;
 import br.com.devplus.forum.repository.CursoRepository;
 import br.com.devplus.forum.repository.TopicoRepository;
-import com.sun.jndi.toolkit.url.Uri;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -30,12 +32,15 @@ public class TopicosController {
     CursoRepository cursoRepository;
 
     @GetMapping
-    public List<TopicoDto> lista(String nomeCurso) {
+    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+                                 @RequestParam int page, @RequestParam int qntd) {
+        Pageable pageable = PageRequest.of(page, qntd);
+
         if (nomeCurso == null) {
-            List<Topico> topicos = topicoRepository.findAll();
+            Page<Topico> topicos = topicoRepository.findAll(pageable);
             return TopicoDto.converter(topicos);
         } else {
-            List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+            Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, pageable);
             return TopicoDto.converter(topicos);
         }
     }
