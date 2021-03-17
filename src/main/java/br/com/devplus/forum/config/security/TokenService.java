6c345c0b383db83ad41d8f3cb586ed703a0b8df5
Gustavo Ridgue <guferrari007@gmail.com>
2021-03-17 11:45:47 -0300

    Generate token throught JWT

---
+++ src/main/java/br/com/devplus/forum/config/security/TokenService.java
@@ -0,0 +1,33 @@
+package br.com.devplus.forum.config.security;
+
+import br.com.devplus.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+    @Value("${forum.jwt.expiration}")
+    private String expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authentication) {
+        Usuario usuarioLogado = (Usuario) authentication.getPrincipal();
+        Date hoje = new Date();
+        Date expiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("DevPlus Forum API")
+                .setSubject(usuarioLogado.getId().toString())
+                .setIssuedAt(hoje)
+                .setExpiration(expiracao)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
---
+++ src/main/java/br/com/devplus/forum/controller/AutenticacaoController.java
@@ -0,0 +1,42 @@
+package br.com.devplus.forum.controller;
+
+import br.com.devplus.forum.config.security.TokenService;
+import br.com.devplus.forum.controller.form.LoginForm;
+import br.com.devplus.forum.controller.dto.TokenDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(form.getEmail(), form.getSenha());
+
+        try {
+            Authentication authentication = authManager.authenticate(authenticationToken);
+            String token = tokenService.gerarToken(authentication);
+
+            return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
